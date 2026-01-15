4a88d4b19fbdadbf36cb346e7a175e742d59e702
Dela Deliansyah <dela10223058@student.sttcipasung.ac.id>
2026-01-15 10:32:37 +0700

    menambahkan seeder, migration, serta model untuk fitur pegaduan di hak akses masyarakat

---
+++ database/migrations/2026_01_15_025003_create_complaints_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('resident_id');
+            $table->string('title');
+            $table->text('content');
+            $table->enum('status', ['new', 'prcessing', 'completed'])->default('new');
+            $table->string('photo_proof')->nullable();
+            $table->timestamp('report_date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
+
+            $table->foreign('resident_id')->references('id')->on('residents')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Resident;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,11 +17,32 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
+            'id' => 1,
             'name' => 'Admin SiRT',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
             'status' => 'approved',
             'role_id' => '1', //=> 'Admin'
+        ]);
+
+        User::create([
+            'id' => 2,
+            'name' => 'Penduduk 1',
+            'email' => 'penduduk1@gmail.com',
+            'password' => Hash::make('password'),
+            'status' => 'approved',
+            'role_id' => '2', //=> 'User'
+        ]);
+
+        Resident::create([
+            'user_id' => 2,
+            'nik' => '0987654332211223',
+            'nama' => 'Adam',
+            'gender' => 'laki-laki',
+            'tanggal_lahir' => '2005-01-01',
+            'tempat_lahir' => 'Tasikmalaya',
+            'alamat' => 'Tasikmalaya',
+            'status_perkawinan' => 'belum_menikah',
         ]);
     }
 }
